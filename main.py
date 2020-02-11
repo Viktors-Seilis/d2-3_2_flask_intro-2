f8bfe22ee03226b73024bd90a328f197a81b294d
Viktors-Seilis <vikijs10@gmail.com>
2020-02-11 06:03:29 +0000

    New commit

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "Hi!"
+  return "Sveikiņi!"
 
 @app.route('/home')
-def home() :
-  return render_template('home.html', aktiva_lapa = 'home')
+def home():
+  return render_template('home.html')
 
 @app.route('/about')
-def about() :
+def about_html():
   return render_template('about.html')
 
-@app.route('/contacts')
-def contacts() :
-  return render_template('contacts.html')
+@app.route('/contact')
+def contacts_html():
+  return render_template('contact.html', phone = "22140077")
 
 @app.route('/params')
 def params():
-  args = request.args
- 
-  return render_template('params_table.html', args = args )
+  return request.args
+
+@app.route('/post', methods = ['POST'])
+def post():
+  return request.get_json()
+
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  content_type = request.content_type
+  if content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Add line {contentJSON['data']} to file."
+  else:
+    return f"Content type {content_type} is not supported!"
+  
+@app.route('/file', methods = ['GET', 'POST'])
+def workFile():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} is not supported!"
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 5555 ,
-  threaded = True, debug = True )
+  app.run(host="0.0.0.0", threaded=True, port=5020, debug=True)
