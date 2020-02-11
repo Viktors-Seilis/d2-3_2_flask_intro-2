8a06713f1a6df62306a935d3494fd6db6fd31402
[Students] Viktors Valdis Seilis <83DVVSeilis@rvt.lv>
2020-02-11 08:23:19 +0200

    stradā2

--- file_proc.py
+++ file_proc.py
@@ -1,7 +1,7 @@
 # a - Will append to the end of the file
 # w - Will overwrite any existing content
 
-file_name = 'data.txt'
+file_name = '.\d2-3_2_flask_intro-2\data.txt'
 
   # with open(file_name, 'w') as f:       
   #     f.write(line)
--- main.py
+++ main.py
@@ -53,3 +53,4 @@ def workFile():
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", threaded=True, port=5020, debug=True)
+print(os.getcwd())
